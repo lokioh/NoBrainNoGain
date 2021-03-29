ba6ab32fff9d151f8d982d14046e59c520936058
lokioh <loiclim.etu@gmail.com>
2021-03-30 01:27:58 +0200

    test db

--- models/NoBrainNoGainDB.sql
+++ models/NoBrainNoGainDB.sql
@@ -1,7 +1,7 @@
 -- Création des tables
 
-CREATE TABLE IF NOT EXISTS User(
-    id_user INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS User (
+    id_user INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name_user TEXT NOT NULL,
     mail_user TEXT NOT NULL,
     pwd_user TEXT NOT NULL,
@@ -14,7 +14,7 @@ CREATE TABLE IF NOT EXISTS User(
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE IF NOT EXISTS Games (
-    id_games INT(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_games INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name_games TEXT NOT NULL,
     rate_use_games INT NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -22,10 +22,16 @@ CREATE TABLE IF NOT EXISTS Games (
 -- Insertion des données dans la bdd
 
 INSERT INTO Games (id_games, name_games, rate_use_games) VALUES
-(1, 'sudoku', 0),
-(2, 'chess', 0),
-(3, 'dames', 0),
-(4, 'connect4', 0);
+(1, 'sudoku', 70),
+(2, 'chess', 100),
+(3, 'dames', 54),
+(4, 'connect4', 124);
 
-INSERT INTO User (id_user, name_user, mail_user, pwd_user) VALUES
-(1, 'chomeur', 'test@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG');
+INSERT INTO User (id_user, name_user, mail_user, pwd_user, about_user, score_user, score_chess_user, score_connect4_user, score_dames_user, score_sudoku_user) VALUES
+(1, 'Loïc', 'loic@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG', ' ', 200, 60, 40, 50, 50),
+(2, 'Vincent', 'vincent@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG', ' ', 160, 35, 45, 40, 40),
+(3, 'Aravindan', 'aravindan@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG', ' ', 120, 25, 20, 35, 20),
+(4, 'Bilal', 'bilal@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG', ' ', 120, 20, 35, 20, 25),
+(5, 'Tom', 'tom@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG', ' ', 80, 0, 40, 20, 20),
+(6, 'Victor', 'victor@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG', ' ', 75, 0, 40, 20, 15),
+(7, 'Invité', 'invite@mail.com', '$2b$10$H69/mgaqxg6NzosxEriK9.Uw8a./t0l01dmHqTwKszuY/CjTwE7sG', ' ', 0, 0, 0, 0, 0),
